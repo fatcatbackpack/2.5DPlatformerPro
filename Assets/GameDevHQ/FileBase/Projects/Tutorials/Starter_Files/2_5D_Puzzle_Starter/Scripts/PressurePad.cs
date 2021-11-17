b4e6921cac653aa621303c2950b88bca378f9786
Alec <aleconey@gmail.com>
2021-11-17 16:27:53 -0700

    pressure pad

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/PressurePad.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "MovingBox")
+        {
+            float distance = Vector3.Distance(transform.position, other.transform.position);
+            Debug.Log(distance);
+
+            if (distance < 0.05f)
+            {
+                Rigidbody box = other.GetComponent<Rigidbody>();
+                if (box != null)
+                {
+                    box.isKinematic = true;
+                }
+                MeshRenderer renderer = GetComponentInChildren<MeshRenderer>();
+                if (renderer != null)
+                {
+                    renderer.material.color = Color.blue;
+                }
+                Destroy(this);
+            }
+
+        }
+    }
+    //when close to center
+    //dis rigidbody or set to kinematic
+    //change color
+
+
+}
